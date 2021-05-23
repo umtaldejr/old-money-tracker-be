3f02bce12a72e401aeb63a9d027ee372ffb2b3af
Exupério Lédo Silva Júnior <uspjr.95@hotmail.com>
2021-05-23 14:12:09 -0300

    Set up ESLint to work with Typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,26 @@
 module.exports = {
+  root: true,
   env: {
     es2021: true,
     node: true,
     jest: true,
   },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
   extends: [
-    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
+    'quotes': ['error', 'single'],
+    '@typescript-eslint/indent': ['error', 2],
   },
 };
